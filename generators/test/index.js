8c2f7614e8ae90053af492dc695ecdda9a193cad
Mattia Franchetto <mt.franchetto@gmail.com>
2018-12-17 17:00:02 +0100

    Refactor test generator

--- generators/test/index.js
+++ generators/test/index.js
@@ -24,43 +24,50 @@ module.exports = class extends Generator {
   }
 
   install() {
-    let dependencies = [];
-    let scripts = {};
-    let types = "";
+    let depsAndScripts = depsAndScriptsFor(this.answers.testRunner, this.answers.testFiles);
 
-    if (this.answers.testRunner === "mocha") {
-      dependencies = [
-        "ts-node@^7.0.1",
-        "mocha@^5.2.0",
-        "@types/mocha@^5.2.5"
-      ];
-      types = "mocha";
-      scripts = {
-        "test": `mocha -r ts-node/register "${this.answers.testFiles}"`,
-        "test-watch": `mocha -r ts-node/register --watch --watch-extensions ts,tsx "${this.answers.testFiles}"`
-      }
-    } else {
-      dependencies = [
-        "jest@^23.6.0",
-        "ts-jest@^23.10.5",
-        "@types/jest@^23.3.10"
-      ];
-      types = "jest";
-      scripts = {
-        "test": "jest",
-        "test-watch": "jest --watch",
-        "coverage": "jest --coverage"
-      }
+    if (this.answers.testRunner === "jest") {
       this.fs.copy(
         this.templatePath("jest.config.js"),
         this.destinationPath("jest.config.js")
       );
     }
 
-    this.fs.extendJSON(this.destinationPath("tsconfig.json"), defaultTSConfig(types));
-    this.npmInstall(dependencies, { "save-dev": true });
+    this.fs.extendJSON(this.destinationPath("tsconfig.json"), defaultTSConfig(depsAndScripts.types));
+    this.npmInstall(depsAndScripts.dependencies, { "save-dev": true });
     extendPackage({
-      scripts: scripts
+      scripts: depsAndScripts.scripts
     }, this);
   }
 };
+
+function depsAndScriptsFor(testRunner, testFiles) {
+  if (testRunner === "mocha") {
+    return {
+      dependencies: [
+        "ts-node@^7.0.1",
+        "mocha@^5.2.0",
+        "@types/mocha@^5.2.5"
+      ],
+      types: "mocha",
+      scripts: {
+        "test": `mocha -r ts-node/register "${testFiles}"`,
+        "test-watch": `mocha -r ts-node/register --watch --watch-extensions ts,tsx "${testFiles}"`
+      }
+    };
+  } else {
+    return {
+      dependencies: [
+        "jest@^23.6.0",
+        "ts-jest@^23.10.5",
+        "@types/jest@^23.3.10"
+      ],
+      types: "jest",
+      scripts: {
+        "test": "jest",
+        "test-watch": "jest --watch",
+        "coverage": "jest --coverage"
+      }
+    }
+  }
+}
